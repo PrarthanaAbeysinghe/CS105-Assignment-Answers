44521f76f16a0458583e0b347ffbcdd806da540a
PrarthanaAbeysinghe <55149271+PrarthanaAbeysinghe@users.noreply.github.com>
2019-12-17 10:23:53 +0530

    add assignment_08 answers

---
+++ Assignment_08/Individual_Assignment_08/Fillable.java
@@ -0,0 +1,8 @@
+/**
+ * The Fillable interface defines methods used to fill an object.
+ */
+public interface Fillable
+{
+    public boolean isFilled();
+    public void setFilled(Boolean filled);
+}
---
+++ Assignment_08/Individual_Assignment_08/Scalable.java
@@ -0,0 +1,9 @@
+/**
+ * The Scalable interface defines methods used in scaling an object.
+ */
+public interface Scalable
+{
+    public void scale(double sf);
+
+    public void scale(double sx, double sy);
+}
